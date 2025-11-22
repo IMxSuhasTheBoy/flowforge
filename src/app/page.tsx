64d7018f1ab8e33d6de04a56d73bed798319b231
IMxSuhasTheBoy <dannylivecsr2@gmail.com>
2025-11-22 13:50:56 +0530

    02: database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
-const Page = () => {
+import prisma from "@/lib/db";
+
+const Page = async () => {
+  const users = await prisma.user.findMany();
+
   return (
     <div className="text-3xl h-full min-h-screen min-w-screen flex items-center justify-center">
       FlowForge Studio
+      {JSON.stringify(users)}
     </div>
   );
 };
